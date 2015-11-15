2dd04ddf9bfaf620b5a4075bbc70e9a03df70f05
Michael Herman <hermanmu@gmail.com>
2015-11-15 15:16:30 -0700

    updated styles, added boilerplate login page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,14 +5,18 @@ var passport = require('../auth/github.js');
 
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressJS' });
+  res.render('index', { title: 'home' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
 });
 
 router.get('/auth/github',
   passport.authenticate('github', { scope: [ 'user:email' ] }));
 
 router.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
+  passport.authenticate('github', { failureRedirect: '/login' }),
   function(req, res) {
     res.json('success!');
   });
