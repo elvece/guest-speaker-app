2a6de5a71ef8d5c3bc3cce3907a8156ce865cc5f
Michael Herman <hermanmu@gmail.com>
2015-11-16 18:12:24 -0700

    added passport mocking

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -2,25 +2,25 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     email: {
-        type: DataTypes.STRING,
-        required: true,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
+      type: DataTypes.STRING,
+      required: true,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
     },
     admin: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-        required: true,
-        allowNull: false
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      required: true,
+      allowNull: false
     },
     cohort: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true
     },
     picture: {
-        type: DataTypes.STRING
+      type: DataTypes.STRING
     }
   }, {
     classMethods: {
--- test/routes/index.js
+++ test/routes/index.js
@@ -4,20 +4,60 @@ var mocha = require('mocha');
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../../src/server/app.js');
+var models = require('../../src/server/models/index');
+var passportStub = require('passport-stub');
 
+passportStub.install(server);
 var should = chai.should();
 chai.use(chaiHttp);
 
+
 describe('index routes', function() {
+
+  beforeEach(function(done){
+    models.User.sync({
+      force: true
+    }).then(function() {
+      models.User.create({
+        email: 'michael@herman.com',
+        admin: false
+      }).then(function(user) {
+        passportStub.login(user);
+        done();
+      });
+    });
+  });
+
+  afterEach(function(done){
+    models.User.sync({
+      force: true
+    }).then(function() {
+      done();
+    });
+  });
+
   it('should contain a `/` route', function(done) {
     chai.request(server)
     .get('/')
     .end(function(err, res) {
       res.should.have.status(200);
       res.text.should.have.string('<p>Welcome');
+      res.text.should.have.string('<li><a href="/auth/logout">Logout</a></li>');
       done();
     });
   });
+
+  it('should behave differently when a user is logged in', function(done) {
+    chai.request(server)
+      .get('/')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.html;
+        res.text.should.have.string('<li><a href="/auth/logout">Logout</a></li>');
+        done();
+      });
+  });
+
   it('should handle 404 errors', function(done) {
     chai.request(server)
     .get('/doesnotexist')
@@ -27,4 +67,5 @@ describe('index routes', function() {
       done();
     });
   });
+
 });
